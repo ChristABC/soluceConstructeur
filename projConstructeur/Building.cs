093675c96fb3db0b2fa1604967bfa01b7e225a9d
Christ ASSANA <christassana@gmail.com>
2024-06-21 15:29:57 +0200

    Ajoutez des fichiers projet.

---
+++ projConstructeur/Building.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projConstructeur
+{
+    internal class Building
+    {
+        private int _floors;
+        private double _height;
+
+        // Constructeur qui initialise le bâtiment avec le nombre d'étages et la hauteur
+        public Building(int floors, double height)
+        {
+            this._floors = floors;
+            this._height = height;
+        }
+        public Building(int floors) 
+        {
+            this._floors = floors;
+            this._height=3*this._floors;
+
+        }
+
+        public int Floors
+        {
+            get { return _floors; }
+            set { _floors = value; }
+        }
+
+        public double Size
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+        public int GetFloorMaxSize()
+        {
+            return (int)(_height/_floors);
+        }
+
+        public double Height()
+        {
+            return _height;
+        }
+    }
+}
---
+++ projConstructeur/Program.cs
@@ -0,0 +1,18 @@
+﻿using projConstructeur;
+using System;
+
+class MainProgram
+{
+        public static void Main(string[] args) {
+
+        Console.WriteLine("Quelle est le nombre d'étage de l'immeuble en mètres? ");
+        double hauteur=Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Connaisez vous "
+
+
+
+
+        }
+ 
+}
